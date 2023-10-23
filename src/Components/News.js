1f881d181719f63a5de7094fac9a6f86ad6f82ad
Faisal Misbah <132295428+FaisalMisbah23@users.noreply.github.com>
2023-10-23 15:53:57 +0500

    First

---
+++ src/Components/News.js
@@ -0,0 +1,105 @@
+import React , { Component } from 'react'
+import NewsItem from './NewsItem'
+import Spinner from './Spinner'
+import PropTypes from 'prop-types'
+
+
+export class News extends Component {
+    static defaultProps = {
+        country:"in" ,
+        pagesize: 10 ,
+        category: "general"
+    }
+    static propTypes = {
+        country: PropTypes.string, 
+        pagesize:PropTypes.number,
+        category:PropTypes.string
+    }
+    Capilizedfirstletter=(string) =>{
+        return string.charAt(0).toUpperCase() + string.slice(1)
+    }
+
+    constructor(props){
+        super(props)
+        console.log("I am a Constructor")
+        this.state = {
+        articles:[],
+        loading:false,
+        }
+        document.title = `${this.Capilizedfirstletter(this.props.category)} - Daily News`
+    }
+    async componentDidMount(){
+        console.log("ABC")
+        this.props.setProgress(0);
+         // eslint-disable-next-line
+         let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=9cd21b678a9e426eafd9a28858e9e298&page=${this.state.page = 1}&pagesize=${this.props.pagesize}`;
+         this.setState({loading:true})
+        let data= await fetch(url)
+        let parsedData= await data.json()
+        console.log(parsedData)
+        this.setState({articles: parsedData.articles , totalArticles:parsedData.totalResults , loading:false})
+        this.props.setProgress(100);
+    }
+
+    NextPage= async()=>{
+        this.props.setProgress(0);
+        if(!(this.state.page + 1 > Math.ceil(this.state.totalArticles/this.props.pagesize))){    
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=9cd21b678a9e426eafd9a28858e9e298&page=${this.state.page + 1}&pagesize=${this.props.pagesize}`;
+       this.setState({loading:true})
+       this.props.setProgress(30);
+        let data= await fetch(url)
+        let parsedData= await data.json()
+        console.log(parsedData)
+        this.setState({
+        page:this.state.page + 1,
+        articles: parsedData.articles,
+        loading:false
+        
+    })
+        this.props.setProgress(100);
+        }}
+       
+        
+    PreviousPage= async ()=>{
+        
+        console.log("Previous")
+        this.props.setProgress(0);
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=9cd21b678a9e426eafd9a28858e9e298&page=${this.state.page - 1}&pagesize=${this.props.pagesize}`;
+        this.setState({loading:true})
+        this.props.setProgress(20);
+        let data= await fetch(url)
+        let parsedData= await data.json()
+        console.log(parsedData)
+        this.setState({
+            page:this.state.page - 1,
+            articles: parsedData.articles,
+            loading:false
+        })
+        this.props.setProgress(100);
+    }
+  render() {
+    return (
+      <div className='container my-3'>
+        <h1 style={{marginTop:"70px"}}> Daily News -Top {this.Capilizedfirstletter(this.props.category)} Headlines </h1>
+       {  this.state.loading &&  <Spinner/>}
+        <div className="row">
+        {!this.state.loading && this.state.articles.map((element)=>{
+            return  <div className="my-3 col-md-4" key={element.url} >
+   <NewsItem title={element.title } description={element.description} urlToImage={element.urlToImage} url={(element.url)} author={element.author} date={element.publishedAt} source={element.source.name} content={element.source.id} />
+  
+   </div>
+
+            
+        })}
+     
+     </div>
+        <div className="container d-flex justify-content-between">
+        <button disabled={this.state.page<=1} type="button" onClick={this.PreviousPage} className="btn btn-dark">&laquo; Previous</button>
+        <button disabled={this.state.page + 1 > Math.ceil(this.state.totalArticles/this.props.pagesize)} type="button" onClick={this.NextPage} className="btn btn-dark">Next &raquo;</button>
+        </div>
+      </div>
+    )
+  
+}}
+
+export default News
---
+++ src/Components/NewsItem.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+// import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+
+export class NewsItem extends Component {
+  render() {
+    let {title, description,urlToImage,url,author,date,source} = this.props;
+    return (
+      <>
+      <div className="card" style={{width: "18rem"}}>
+      <div style={{
+      display: "flex",
+    justifyContent: "flex-end",
+    right: "0",
+    position: "absolute",
+    }} >
+    <span class=" badge rounded-pill bg-danger" style={{left: '90%',zindex: "1"}} >
+    {source}
+  </span></div>
+  <img src={!urlToImage?"https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg":urlToImage} className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">{title}</h5>
+  
+    <p className="card-text">{description}</p>
+    <p className="card-text"><small class="text-muted">By {!author?"Unknown":author} on {new Date(date).toGMTString()} </small></p>
+    {/* eslint-disable-next-line */}
+    <a href={url} target="_blank" className="btn btn-dark">Read More</a>
+  </div>
+</div>
+</>
+    )
+  }
+}
+
+export default NewsItem
